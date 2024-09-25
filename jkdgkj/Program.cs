3171c957f6c6dbeb383a0e7b1f35714ab6e5ebdd
Erik836-web <erikkocsis2005@gmail.com>
2024-09-25 08:02:30 +0200

    ahaha

--- jkdgkj/Program.cs
+++ jkdgkj/Program.cs
@@ -4,13 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace jkdgkj
+namespace test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //ide kódok jönnek
+            int sum = 0;
+            int paros = 0;
+            int paratlan = 0;
+
+            while (true)
+            {
+                Console.Write("Adj meg egy számot: ");
+                int szam = Convert.ToInt32(Console.ReadLine());
+
+                sum += szam;
+
+                if (szam % 2 == 0)
+                {
+                    paros++;
+                }
+                else
+                {
+                    paratlan++;
+                }
+
+                if (sum > 100)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"a számok összege: {sum}");
+            Console.WriteLine($"Páros számok: {paros}");
+            Console.WriteLine($"Páratlan számok: {paratlan}");
+            Console.ReadLine();
         }
     }
 }
