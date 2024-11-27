1c0f51ce2fcb150599c0afe4f5593ab2538d0b05
Erik836-web <erikkocsis2005@gmail.com>
2024-11-27 10:07:40 +0100

    kivitel

---
+++ kivetel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kivetel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //osztály példányosítása
+            kivetelclass k = new kivetelclass();
+            k.kivKezOs();
+        }
+    }
+}
---
+++ kivetel/kivetelclass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kivetel
+{
+    class kivetelclass
+    {
+        public kivetelclass() { }
+        //nincs kivétel kezelés
+        public void kivKezOs()
+        {
+            Console.WriteLine("nincs kivételezés\nAdjon meg egy karaktert!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("a beírt szám {0}", szam);
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e . ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
