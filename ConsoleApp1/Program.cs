0a399cd676e2caad6e03a0974aadddf2857cb275
Erik836-web <erikkocsis2005@gmail.com>
2025-02-05 10:25:52 +0100

    sad

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstarct
+{
+    /*ez a példa az abstract kulcsszó használatát mutatja be az absztract kulcsszóval olyan osztályokat és metódusokat jelölünk, melyeket nem lehet közvetlenül példányosítani, illetve végrahajtani. Az absztract osztályokat alapként használják más osztályok számára, még az absztract metódusok deklarációkat, de nem adnak impelemntácíókat. 
+     Fontos jellemzők:
+    - absztract osztály: csak származtatott osztályokon keresztül használható. Tartalmazhat absztract és nem absztract (működő) metódusokat is.
+    - absztract metódusok: csak absztract osztályban lehet, és a származtatott osztálynak kötelező megvalósítania.*/
+    abstract class Program
+    {
+        //abstract metódus - nincs implamentáció
+        // ennek a kódját a leszármazottban kötelező megírni
+        public abstract double GetArea();
+
+        // Normal metódus - van implamentáció
+
+        public void DisplayShape();
+        {
+            console.writelane("ez egy alakzat);
+        }
+    }
+        class Circle : Shape
+        {
+            private double suger;
+
+            public Circle(double r)
+            {
+                this.sugar = r;
+            }
+
+            //Abstract metódus implamentálása
+
+            public override double GetArea();
+            {
+                return Math.PI* sugar;
+            }
+        }
+}
